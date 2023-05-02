aa206026fbdd5c561a0405230f0970a5b684d058
King <kelkab46@gmail.com>
2023-05-02 07:48:59 +0300

    completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import React, { useState, useEffect, useLocation } from 'react';
+import LandingPage from "./pages/LandingPage";
+
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Dashboards from "./pages/Dashboards";
+import PrivateRoutes from "./utils/PrivateRoutes";
+import ModeDashboard from "./pages/ModeDashboard";
+import TwDashboard from "./pages/TwDashboard";
+import ArticleCard from "./components/ArticleCard";
+import Profile from "./components/Profile";
+import Categories from "./components/Categories";
 
 function App() {
+
+  // Keep track of if user is Logged in or not
+  const [loggedIn, setIsLoggedIn] = useState(false);
+
+  // Keep track of the user
+  const [user, setUser] = useState(null);
+
+  // Keep track of user role
+  const [role, setRole] = useState(null);
+
+  const location = useLocation
+
+  useEffect(() => {
+    // auto-login user
+    fetch("http://localhost:3000/me")
+      .then((response) => response.json())
+      .then((data) => {
+        // Update the state with the response data
+        setUser(data);
+        setRole(data.role)
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+
+  const handleLogin = (user) => {
+    setIsLoggedIn(true);
+    console.log(user)
+    sessionStorage.setItem("user", JSON.stringify(user));
+    console.log(loggedIn)
+    setUser(user)
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    // handle successful logout
+    setUser(null); // set user state to null
+    sessionStorage.removeItem("user")
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />}></Route>
+        <Route path="/login" element={<Login handleLogin={handleLogin} />}></Route>
+        <Route path="/signup" element={<Signup handleLogin={handleLogin} />}></Route>
+        <Route element={<PrivateRoutes loggedIn={loggedIn} />}>
+          <Route path="/dashboard" element={<Dashboards user={user} handleLogout={handleLogout} />}></Route>
+          <Route path="/modedashboard" element={<ModeDashboard user={user} handleLogout={handleLogout} />}></Route>
+          <Route path="/twdashboard" element={<TwDashboard user={user} handleLogout={handleLogout} />}></Route>
+          <Route path="/article/:id" element={<ArticleCard user={user} />}></Route>
+          <Route path="/categories" element={<Categories user={user} />}></Route>
+          <Route path="/profile" element={<Profile user={user} />}></Route>
+          
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/pages/TwDashboard.js
@@ -0,0 +1,110 @@
+import React, { useState } from 'react'
+import Control from '../assets/control.png'
+import Logo from '../assets/logo.png';
+import Book from '../assets/Folder.png'
+import Home from '../assets/Home.png'
+import Logout from '../assets/Logout.png'
+import ProfileImage from '../assets/Profile.png'
+import Person from '../assets/User.png'
+import Feed from '../components/Feed';
+import Articles from '../components/Articles';
+import Users from '../components/Users';
+import Profile from '../components/Profile';
+import Categories from '../components/Categories';
+
+function TwDashboard ({ user, handleLogout }) {
+
+    const [open, setOpen] = useState(true)
+    const [activeDashboard, setActiveDashboard] = useState('feed');
+
+    function handleClick(dashboard){
+        setActiveDashboard(dashboard);
+    }
+
+    function handleSignout(){
+        // Logout
+        fetch('http://localhost:3000/logout', {
+          method: 'DELETE',
+        })
+        .then(response => {
+          if (response.ok) {
+            // handle successful logout
+            handleLogout();
+            
+          } else {
+            throw new Error('Logout failed');
+          }
+        })
+        .catch(error => console.error(error));
+    }
+
+
+  return (
+    <div className='flex'>
+        <div className={`${open ? "w-72" : "w-20"} duration-300 h-screen p-5 pt-8 bg-[#101f3c] relative `}>
+            <img
+                src={Control}
+                className={`absolute cursor-pointer rounded-full -right-3 top-9 w-7 border-2 border-[#101f3c] ${!open && 'rotate-180'}`}
+                onClick={() => setOpen(!open)}
+            />
+            <div className='flex gap-x-4 items-center'>
+                <img 
+                    src={Logo} 
+                    className={`cursor-pointer duration-500 ${open && "rotate-[360deg]"}`} 
+                />
+                <h1 className={`text-white origin-left font-medium text-2xl duration-300 ${!open && "scale-0"}`}>MS Daily Dev</h1>
+            </div>
+            {/* Sidebar Menu */}
+            <ul className='pt-12'>
+                <li className={`${activeDashboard === 'feed' ? 'bg-[rgba(255,255,255,0.17)]' : ""} pt-4 pb-4 text-white hover:text-[#F9500D] text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-[rgba(255,255,255,0.17)] rounded-md`} 
+                    onClick={() => handleClick('feed')}
+                >
+                    <img src={Home}/>
+                    <span className={`${!open && 'hidden'} origin-left duration-200`}>Feed</span>
+                </li>
+                <li className={`${activeDashboard === 'articles' ? 'bg-[rgba(255,255,255,0.17)]' : ""} pt-4 pb-4 text-white hover:text-[#F9500D] text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-[rgba(255,255,255,0.17)] rounded-md`} 
+                    onClick={() => handleClick('articles')}
+                >
+                    <img src={Book}/>
+                    <span className={`${!open && 'hidden'} origin-left duration-200`}>Articles</span>
+                </li>
+
+                <li className={`${activeDashboard === 'categories' ? 'bg-[rgba(255,255,255,0.17)]' : ""} pt-4 pb-4 text-white hover:text-[#F9500D] text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-[rgba(255,255,255,0.17)] rounded-md`} 
+                    onClick={() => handleClick('categories')}
+                >
+                    <img src={Person}/>
+                    <span className={`${!open && 'hidden'} origin-left duration-200`}>Categories</span>
+                </li>
+                
+                {/* Logout & Profile */}
+                
+                <li className={`${activeDashboard === 'profile' ? 'bg-[rgba(255,255,255,0.17)]' : ""} pt-4 pb-4 text-white hover:text-[#F9500D] text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-[rgba(255,255,255,0.17)] rounded-md`} 
+                    onClick={() => handleClick('profile')}
+                >
+                    <img src={ProfileImage}/>
+                    <span className={`${!open && 'hidden'} origin-left duration-200`}>Profile</span>
+                </li>
+
+                <li className={`${activeDashboard === 'logout' ? 'bg-[rgba(255,255,255,0.17)]' : ""} pt-4 pb-4 text-white hover:text-[#F9500D] text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-[rgba(255,255,255,0.17)] rounded-md`} 
+                    onClick={() => handleSignout()}
+                >
+                    <img src={Logout}/>
+                    <span className={`${!open && 'hidden'} origin-left duration-200`}>Logout</span>
+                </li>
+
+                </ul>
+        </div>
+        <div className='p-7 text-2xl flex-1 h-screen'>
+            <h1 className='pb-12'>Dashboard</h1>
+            {activeDashboard === 'feed' && <Feed />}
+            {activeDashboard === 'articles' && <Articles />}
+            {activeDashboard === 'users' && <Users />}
+            {activeDashboard === 'categories' && <Categories />}
+            {activeDashboard === 'profile' && <Profile />}
+            
+        </div>
+    </div>
+  )
+}
+
+export default TwDashboard
